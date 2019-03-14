c679bc729bee054fe602de834319fa6cfd382e41
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-03-14 10:45:18 +0100

    Use react-i18next

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,12 +1,41 @@
-import I18n from 'ex-react-native-i18n';
-import en from './languages/en.json';
-import da from './languages/da.json';
-
-I18n.fallbacks = true;
-
-I18n.translations = {
-    en,
-    da
-};
-
-export default I18n;
+//import en from './languages/en.json';
+//import da from './languages/da.json';
+import i18n from 'i18next';
+import Expo from 'expo';
+// creating a language detection plugin using expo
+// http://i18next.com/docs/ownplugin/#languagedetector
+const languageDetector = {
+  type: 'languageDetector',
+  async: true, // async detection
+  detect: (cb) => {
+    return Expo.Util.getCurrentLocaleAsync()
+      .then(lng => { cb(lng.replace('_', '-')); })
+  },
+  init: () => {},
+  cacheUserLanguage: () => {}
+}
+i18n
+  .use(languageDetector)
+  .init({
+    fallbackLng: 'en',
+    // the translations
+    // realworld load that via xhr or bundle those using webpack    
+    resources: {
+      en: {
+        home: {
+          title: 'Welcome''
+        }
+      },
+      de: {
+        home: {
+          title: 'Willkommen'
+        }
+      },
+      // have a initial namespace
+      ns: ['translation'],
+      defaultNS: 'translation',
+      interpolation: {
+        escapeValue: false // not needed for react
+      }
+  });
+export default i18n;
