27448c8dd105aa7480f085fb33fcac145c9708ae
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-03-16 17:09:03 +0100

    Add header to CounterScreen

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,23 +1,28 @@
 import AppComponent from "../components/AppComponent";
-import {
-  Body,
-  Button,
-  Header,
-  Icon,
-  Left,
-  Right,
-  Title,
-  Container,
-  Label
-} from "native-base";
+import { Header, Title, Body, Container } from "native-base";
+import { StyleSheet } from "react-native";
 import React from "react";
+import i18n from "i18n-js";
 
 export class CounterScreen extends React.Component {
   render(): JSX.Element {
     return (
-      <Container>
+      <Container style={styles.container}>
+        <Header style={{ backgroundColor: "#03426A" }}>
+          <Body>
+            <Title style={{ color: "#fff", width: "100%" }}>
+              {i18n.t("foo")}
+            </Title>
+          </Body>
+        </Header>
         <AppComponent />
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
